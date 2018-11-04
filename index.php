ff7c7a1fbb233c173bc168a286b4e252150b448c
ashkan <ashkan@gmail.com>
2018-11-04 21:22:44 +0330

    all files added

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>فروشگاه اینترنتی</title>
+</head>
+
+<body style="margin:0; padding:0;">
+
+<div id="line1" style="width:100%; height:4px; background:#006;"></div><!--line1-->
+
+<style>
+#ertebat a{ font-family:tahoma; font-size:12px; margin:0 6px; float:right; }
+
+#ertebat span{float:right;} 
+
+</style>
+
+
+<div id="ertebat" style="width:1100px; height:37px; margin:0 auto;  background:#0F9; text-align:right; direction:rtl; line-height:35px;">
+
+
+<a>بازگشت به صفحه نخست</a>
+<span>|</span>
+<a>درباره ما</a>
+<span>|</span>
+<a>تماس با ما</a>
+
+<a style="margin-right:180px; margin-top:-10px;"> <img src="img/logo.png"></a>
+
+
+
+
+</div><!--ertebat-->
+
+
+
+
+</body>
+
+
+
+</html>
